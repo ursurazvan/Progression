d7b9a7d339dc99f44ce6b95e99a4b2ec2d9de902
ursurazvan <razvan.ursu@student.uclouvain.be>
2018-06-20 20:24:23 +0200

    Second

--- root.py
+++ root.py
@@ -1,3 +1,9 @@
+from subprocess import Popen, PIPE, call
+
+process = Popen(["curl","www.google.com"], stdin = PIPE)
+output = process.stdout.readline()
+
+from subprocess import Popen, PIPE, call
 
 process = Popen(["curl","www.google.com"], stdin = PIPE)
 output = process.stdout.readline()
